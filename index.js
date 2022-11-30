b23a3f2fe701786ee9de6e98bff15f5f68c2d542
Cosette Tshibanda <cosettetshibanda@gmail.com>
2022-11-30 14:41:58 -0700

    commit tree 92932cb7e9fb6201e35347bcd6af8d51850c815e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity){
+    return "This Saturday, I want to " + activity + "!"}
+    else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+const mondayWork = function (activity){
+    if(activity){
+        return "This Monday, I will " + activity + "."
+    }
+    else {
+        return "This Monday, I will go to the office."  
+    }
+
+}
+function wrapAdjective(string = '*'){
+    console.log("unknown",string)
+   return function pizza(name = "special"){
+    return 'You are ' + string + name + string + '!'
+   }
+}
+ // const part1 = function(name = "special") {
+    //     return function 'You are' + name + '!'
+    // }
+    // const part2 = function(symbol = "*"){
+    // }
